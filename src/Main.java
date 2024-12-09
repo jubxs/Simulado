9c2a42898a6ba5ed8ef8e1275e4c134d0915d2d8
Ana Julya Ferreira <anajulyawff@gmail.com>
2024-12-09 16:42:18 -0300

    Criterio 1 e 4 corretos

--- src/Main.java
+++ src/Main.java
@@ -57,8 +57,26 @@ public class Main {
 
         }
          if (criterio4){
+             System.out.println("A questão é de qual área do conhecimento:");
+             System.out.println("Digite 1 -> Linguagens");
+             System.out.println("Digite 2 -> Humanas");
+             System.out.println("Digite 3 -> Exatas");
+             System.out.println("Digite 4 -> Natureza");
+             areadaquestao = in.nextInt();
+             if (areadaquestao == 1){TqLINGUAGENS +=1; if (qerradaouceta ==1){TqELINGUAGENS += 1;}}
+             if (areadaquestao == 2){TqHUMANAS +=1; if (qerradaouceta ==1){TqEHUMANAS +=1;}}
+             if (areadaquestao == 3){TqEXATAS +=1; if (qerradaouceta ==1){TqEEXATAS +=1;}}
+             if (areadaquestao == 4){TqNATUREZA +=1; if (qerradaouceta ==1){TqENATUREZA +=1;}}
 
          }
-        System.out.println("Questões"+ Tqerrada+"/"+Questoes);
+
+        if (criterio1){System.out.println("Questões"+ Tqerrada+"/"+Questoes);}
+        if (criterio2){
+            System.out.println("Questões de Linguagens:"+ (TqLINGUAGENS - TqELINGUAGENS) +"/"+ TqLINGUAGENS);
+            System.out.println("Questões de Humanas:"+ (TqHUMANAS - TqEHUMANAS) +"/"+ TqHUMANAS);
+            System.out.println("Questões de Natureza:"+ (TqNATUREZA - TqENATUREZA) +"/"+ TqNATUREZA);
+            System.out.println("Questões de Exatas"+ (TqEXATAS - TqEEXATAS) + "/"+ TqEXATAS);
+        }
+
     }
 }
